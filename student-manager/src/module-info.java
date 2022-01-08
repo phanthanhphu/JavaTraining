7e2c13cafdee13961c155ac327ed547eb6575dd3
your name <ptp2656@gmail.com>
2022-01-08 11:28:36 +0700

    upload files

---
+++ student-manager/src/module-info.java
@@ -0,0 +1,8 @@
+module com {
+	exports controller;
+	exports view;
+	exports model;
+	exports repository;
+
+	requires java.naming;
+}
---
+++ student-manager/src/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import java.util.ArrayList;
+
+import model.Student;
+
+public class StudentRepository {
+	public ArrayList<Student> list = new ArrayList<>();
+}
